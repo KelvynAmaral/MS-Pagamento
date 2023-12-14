cefe3bd07d5890bb0ff28f87317a943932ffb853
Kelvyn <dev.kelvynjohnson@gmail.com>
2023-12-14 09:14:11 -0300

    Migration

--- pagamentos/src/main/java/br/com/food/pagamentos/model/DadosPagamento.java
+++ pagamentos/src/main/java/br/com/food/pagamentos/model/DadosPagamento.java
@@ -1,24 +1,29 @@
 package br.com.food.pagamentos.model;
 
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.math.BigDecimal;
+
 @Getter
 @Setter
-public record DadosPagamento(
-        @NotBlank
-        Long id,
-        @NotBlank
-        String nome,
+public class DadosPagamento{
+        private Long id;
 
-        @NotBlank
-        String numeroCartao,
 
-        @NotBlank
-        String expiracaoCartao,
-
-        @NotBlank
-        String codigoSegurancaCartao) {
+        private BigDecimal valor;
+        private String nome;
+        private String numeroCartao;
+        private String expiracaoCartao;
+        private String codigoSegurancaCartao;
+        private Status status;
+        private Long pedidoId;
+        private Long formaPagamentoId;
 
 }
--- pagamentos/src/main/java/br/com/food/pagamentos/repository/PagamentoRepository.java
+++ pagamentos/src/main/java/br/com/food/pagamentos/repository/PagamentoRepository.java
@@ -3,6 +3,6 @@ package br.com.food.pagamentos.repository;
 import br.com.food.pagamentos.model.Pagamento;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public class PagamentoRepository implements JpaRepository<Pagamento, Long> {
+public interface PagamentoRepository extends JpaRepository<Pagamento, Long> {
 
 }
